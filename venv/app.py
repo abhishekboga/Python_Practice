80d7e3ec27e6446c93ad106947534b9947042587
bogaa <abhi999.abhishek@gmail.com>
2023-03-26 21:03:52 -0500

    Initial commit

---
+++ venv/app.py
@@ -0,0 +1,8 @@
+is_good_credit = False
+house_price = 1000000
+if is_good_credit:
+    down_payment = house_price * 0.1
+else:
+    down_payment = house_price * 0.2
+print(f"Down Payment:  {down_payment} ")
+
